9aeb7669b50c56cf65b23063f2f7b0b6e95f1afa
Gabriela <gabilana64@hotmail.com>
2023-06-13 21:11:43 +0100

    added banner props

--- webparts/src/webparts/banner/components/IBannerProps.ts
+++ webparts/src/webparts/banner/components/IBannerProps.ts
@@ -1,7 +1,56 @@
+import { DisplayMode } from '@microsoft/sp-core-library';
+import { ISearchResult } from '@pnp/sp/search';
+//import { IImage } from '../assets/welcome-dark.png';
+
+export interface IHighlightsManagementItem {
+  order: number;
+  path: string;
+}
+
 export interface IBannerProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
+  displayMode: DisplayMode;
+  urlCode: string;
+  sharedCode: string;
+}
+
+export interface IBannerItem {
+  title: string;
+  path: string;
+  category?: string;
+  tags?: ITags[];
+  lead?: string;
+  //image: IImage;
+  publishDate: Date;
+  contentTypeName: string;
+}
+
+interface ITags {
+  Label: string;
+  TermID: string;
+}
+
+export interface ITrackerProps {}
+
+export interface ITrackerWebPartProps {}
+
+export interface ITrackModel {
+  PageUrl: string;
+  PageTitle: string;
+  ContentTypeName: string;
+  Category: string;
+  Source: number;
+  Action: number;
+}
+
+export interface ISearchResultBanner extends ISearchResult {
+  LeadOWSMTXT: string;
+  TagsRefStr: string;
+  CategoryNameOWSTEXT: string;
+  ImageOWSIMGE: string;
+  ThumbnailImageOWSIMGE: string;
+  PublicationDateOWSDATE: string;
+  BannerStartDateOWSDATE: string;
+  BannerEndDateOWSDATE: string;
+  ContentTypeNameOWSTEXT: string;
+  LinkOWSURLH: string;
 }
