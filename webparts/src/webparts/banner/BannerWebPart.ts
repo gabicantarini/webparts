c2f059586e63a440fb4df0b08d5d2bc612e8adc6
Gabriela <gabilana64@hotmail.com>
2023-06-14 23:44:16 +0100

    added banner webaprt

--- webparts/src/webparts/banner/BannerWebPart.ts
+++ webparts/src/webparts/banner/BannerWebPart.ts
@@ -1,114 +1,70 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  WebPartContext
+} from '@microsoft/sp-webpart-base';
 import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
-
-import * as strings from 'BannerWebPartStrings';
-import Banner from './components/Banner';
+import { sp } from '@pnp/sp';
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
 import { IBannerProps } from './components/IBannerProps';
-
+import { Context } from './components/Context';
+import { Banner } from './components/Banner';
+import { IContextProps } from './components/Entities';
 export interface IBannerWebPartProps {
-  description: string;
+  urlCode: string;
+  sharedCode: string;
+}
+
+export interface IHeaderWebPartProps {
+  wpContext: WebPartContext;
 }
 
 export default class BannerWebPart extends BaseClientSideWebPart<IBannerWebPartProps> {
-
-  private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+  public onInit(): Promise<void> {      
+    sp.setup(this.context);
+    return super.onInit();
+  }
 
   public render(): void {
-    const element: React.ReactElement<IBannerProps> = React.createElement(
-      Banner,
+    const element: React.ReactElement<IBannerProps> = React.createElement(Banner, {
+      displayMode: this.displayMode,
+      urlCode: this.properties.urlCode,
+      sharedCode: this.properties.sharedCode
+    });
+
+    const provider: React.FunctionComponentElement<React.ProviderProps<IContextProps>> = React.createElement(
+      Context.Provider,
       {
-        description: this.properties.description,
-        isDarkTheme: this._isDarkTheme,
-        environmentMessage: this._environmentMessage,
-        hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        children: element,
+        value: {
+          wpContext: this.context
+        }
       }
     );
 
-    ReactDom.render(element, this.domElement);
-  }
-
-  protected onInit(): Promise<void> {
-    return this._getEnvironmentMessage().then(message => {
-      this._environmentMessage = message;
-    });
-  }
-
-
-
-  private _getEnvironmentMessage(): Promise<string> {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
-      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
-        .then(context => {
-          let environmentMessage: string = '';
-          switch (context.app.host.name) {
-            case 'Office': // running in Office
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
-              break;
-            case 'Outlook': // running in Outlook
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
-              break;
-            case 'Teams': // running in Teams
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
-              break;
-            default:
-              throw new Error('Unknown host');
-          }
-
-          return environmentMessage;
-        });
-    }
-
-    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
-  }
-
-  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
-    if (!currentTheme) {
-      return;
-    }
-
-    this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
-
-    if (semanticColors) {
-      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
-      this.domElement.style.setProperty('--link', semanticColors.link || null);
-      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
-    }
-
+    ReactDom.render(provider, this.domElement);
   }
 
   protected onDispose(): void {
     ReactDom.unmountComponentAtNode(this.domElement);
   }
 
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
-
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
         {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
           groups: [
             {
-              groupName: strings.BasicGroupName,
+              groupName: '',
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
+                PropertyPaneTextField('urlCode', {
+                  label: 'Parametro para URL'
+                }),
+                PropertyPaneTextField('sharedCode', {
+                  label: 'Parametro para link copiado'
                 })
               ]
             }
